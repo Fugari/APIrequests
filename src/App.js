f9991012f4e583cbaaf854be1d686c7de76a20ba
Fugari <88063553+Fugari@users.noreply.github.com>
2024-05-02 19:55:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
-import { useEffect, useState } from 'react';
-import styles from './App.module.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { MainPage, Task, NotFound } from './components/index';
 
 function App() {
-  const [todo, setTodo] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then((todoData) => todoData.json())
-      .then((loadedData) => setTodo(loadedData))
-      .finally(() => setIsLoading(false));
-  }, []);
-
+  
   return (
-    <div className={styles.DataContainer}>
-      <h3>ToDo List</h3>
-      {isLoading 
-        ? <div className={styles.loader}></div> 
-        : todo.map(({ id, title }) => (
-          <div key={id}> {title} </div>
-        ))
-      }
-    </div>
+    <Routes>
+      <Route path='/' element={<MainPage />} />
+      <Route path='/task/:id' element={<Task />} />
+      <Route path='*' element={<NotFound />} /> 
+    </Routes>
   );
 }
 
